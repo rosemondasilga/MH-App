79580ac774203f09ae2b802440fbe84e022b14ad
tedashikode <theodoremawuli77@gmail.com>
2024-10-05 23:11:18 +0000

    setup the react frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline flex justify-center text-red-400">
+        Welcome to the Mental Help App!
+      </h1>
+    </>
+  )
+}
+
+export default App
