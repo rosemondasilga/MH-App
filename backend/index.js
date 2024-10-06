3aee1b2c179b2fe1b6b430b6d7325889860e36f0
tedashikode <theodoremawuli77@gmail.com>
2024-10-06 20:44:15 +0000

    remove student file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/student.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/student", records);
 
 // start the Express server
 app.listen(PORT, () => {
