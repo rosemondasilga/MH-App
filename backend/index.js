a7c61d7e197cfe655f4c07e00d7b5557f2f9cccf
tedashikode <theodoremawuli77@gmail.com>
2024-10-06 20:32:15 +0000

    change code to reflect ecmascript standard

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import records from "./routes/student.js";
+
+const PORT = process.env.PORT || 5050;
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/student", records);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
