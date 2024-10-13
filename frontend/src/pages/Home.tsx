c3fa0e04af0f72e31c147c0a1d281822a345a409
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-10-13 05:40:15 +0100

    done with landing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import Header from '../components/Header'; // Adjust the path if necessary
 import Hero from '../components/Hero';
+import Features from '../components/Services';
+import Testimonial from '../components/Testimonial';
+import Community from '../components/Community';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/footer';
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +16,21 @@ const Home: React.FC = () => {
         </div>
      <div>
      <Hero />
+     </div>
+     <div>
+        <Features />
+     </div>
+     <div>
+        <Testimonial />
+     </div>
+     <div>
+        <Community />
+     </div>
+     <div>
+        <Newsletter />
+     </div>
+     <div>
+        <Footer />
      </div>
     
     </div>
