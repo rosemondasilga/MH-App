1b374f3a77122b3a2a5cf284ed3d9bc1c78b676b
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-10-17 07:44:09 +0100

    completed dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-     
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
   )
